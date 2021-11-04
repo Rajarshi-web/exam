df3fb0792ba7eae86a46a62e4b79b60992a88eb7
Rajarshi-web <92776427+Rajarshi-web@users.noreply.github.com>
2021-11-04 21:40:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+     autoplay: true,
+    dots: true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsiveClass:true,
+    dots:false,
+    
+
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+})
